a8da137707a67d945bf59167d1a3b15448cc056e
jimitas <nonajun805@gmail.com>
2023-04-08 14:46:03 +0900

    2けたをかけるかけ算の筆算作成

--- src/24_kake_hissan2.js
+++ src/24_kake_hissan2.js
@@ -1,4 +1,71 @@
+import { selectMenuCreate } from "./set.js";
+import { duplicationCheck } from "./duplicationCheck.js";
+import { columnCalcCreate2Digit } from "./columnCalcCreate.js";
+import { columnCalcCreate3Digit } from "./columnCalcCreate.js";
+import { answerCreate } from "./answerCreate.js";
+import * as se from "./se.js";
+
+//初期設定
+const select_menu_array = ["2けた×2けた", "3けた×2けた"];
+const kigo = "×";
+
 export function step24() {
-  const TBL = document.getElementById("TBL");
-  TBL.innerHTML = "<h1>作成中</h1>";
+  let index = "0";
+  selectMenuCreate(select_menu_array);
+  question_create();
+
+  // セレクトモードの作成・設定
+  select.addEventListener("change", () => {
+    index = select.value;
+    question_create();
+  });
+
+  //問題作成を行うボタンの設置
+  document.getElementById("question").addEventListener("click", () => question_create());
+
+  //問題作成
+  function question_create() {
+    const left_array = []; //式の左の値を格納する
+    const right_array = []; //式の右の値を格納する
+    const answer_array = []; //答えを格納する
+    const check_array = []; //重複をチェックするための配列
+    se.set.currentTime = 0;
+    se.set.play();
+    let a, b, c, ans;
+    //ここに式を記述する。
+    while (check_array.length < 9) {
+      //重複のない式の組合せが必ず9以上になるようにする。
+      switch (index) {
+        case "0": {
+          a = Math.floor(Math.random() * 90 + 10);
+          b = Math.floor(Math.random() * 90 + 10);
+          break;
+        }
+        case "1": {
+          a = Math.floor(Math.random() * 889 + 100);
+          b = Math.floor(Math.random() * 90 + 10);
+          break;
+        }
+      }
+      ans = Math.floor(a * b);
+      const check = +(a * 100 + b); //チェック用の値
+      const result = duplicationCheck(check, check_array);
+
+      if (result) {
+        check_array.push(check);
+        left_array.push(a);
+        right_array.push(b);
+        answer_array.push(ans);
+      }
+    }
+    switch (index) {
+      case "0":
+        columnCalcCreate2Digit(left_array, kigo, right_array, 3);
+        break;
+      case "1":
+        columnCalcCreate3Digit(left_array, kigo, right_array, 3);
+        break;
+    }
+    answerCreate(answer_array);
+  }
 }
--- src/columnCalcCreate.js
+++ src/columnCalcCreate.js
@@ -1,8 +1,9 @@
 const bangou = ["①", "②", "③", "④", "⑤", "⑥", "⑦", "⑧", "⑨", "⑩", "⑪", "⑫", "⑬", "⑭", "⑮"];
 
-export function columnCalcCreate2Digit(left_array, kigo, right_array) {
+export function columnCalcCreate2Digit(left_array, kigo, right_array, row_length) {
   const TBL = document.getElementById("TBL");
   TBL.innerHTML = "";
+  if (row_length === undefined) row_length = 5;
   for (let row = 0; row < 20; row++) {
     const tr = document.createElement("tr");
     for (let col = 0; col < 12; col++) {
@@ -12,16 +13,22 @@ export function columnCalcCreate2Digit(left_array, kigo, right_array) {
     }
     TBL.appendChild(tr);
   }
-  for (let row = 0; row < 5; row++) {
+  for (let row = 0; row < row_length; row++) {
     for (let col = 0; col < 3; col++) {
       TBL.rows[row * 4 + 1].cells[col * 4].innerHTML = bangou[row * 3 + col];
       TBL.rows[row * 4 + 2].cells[col * 4].innerHTML = kigo;
       TBL.rows[row * 4 + 2].cells[col * 4].style.borderBottom = "solid 1px black";
       TBL.rows[row * 4 + 2].cells[col * 4 + 1].style.borderBottom = "solid 1px black";
       TBL.rows[row * 4 + 2].cells[col * 4 + 2].style.borderBottom = "solid 1px black";
+      if (row_length === 4) {
+        TBL.rows[row * 4 + 3].cells[col * 4 + 3].style.height = "20mm";
+      } 
+      if (row_length === 3) {
+        TBL.rows[row * 4 + 3].cells[col * 4 + 3].style.height = "38mm";
+      }
 
-      const a = left_array[col * 5 + row];
-      const b = right_array[col * 5 + row];
+      const a = left_array[row * 3 + col];
+      const b = right_array[row * 3 + col];
 
       if (Math.floor(a / 10) != 0) TBL.rows[row * 4 + 1].cells[col * 4 + 1].innerHTML = Math.floor(a / 10);
       if (Math.floor(b / 10) != 0) TBL.rows[row * 4 + 2].cells[col * 4 + 1].innerHTML = Math.floor(b / 10);
@@ -30,9 +37,10 @@ export function columnCalcCreate2Digit(left_array, kigo, right_array) {
     }
   }
 }
-export function columnCalcCreate3Digit(left_array, kigo, right_array) {
+export function columnCalcCreate3Digit(left_array, kigo, right_array, row_length) {
   const TBL = document.getElementById("TBL");
   TBL.innerHTML = "";
+  if (row_length === undefined) row_length = 5;
   for (let row = 0; row < 20; row++) {
     const tr = document.createElement("tr");
     for (let col = 0; col < 15; col++) {
@@ -42,7 +50,7 @@ export function columnCalcCreate3Digit(left_array, kigo, right_array) {
     }
     TBL.appendChild(tr);
   }
-  for (let row = 0; row < 5; row++) {
+  for (let row = 0; row < row_length; row++) {
     for (let col = 0; col < 3; col++) {
       TBL.rows[row * 4 + 1].cells[col * 5].innerHTML = bangou[row * 3 + col];
       TBL.rows[row * 4 + 2].cells[col * 5].innerHTML = kigo;
@@ -50,9 +58,15 @@ export function columnCalcCreate3Digit(left_array, kigo, right_array) {
       TBL.rows[row * 4 + 2].cells[col * 5 + 1].style.borderBottom = "solid 1px black";
       TBL.rows[row * 4 + 2].cells[col * 5 + 2].style.borderBottom = "solid 1px black";
       TBL.rows[row * 4 + 2].cells[col * 5 + 3].style.borderBottom = "solid 1px black";
+      if (row_length === 4) {
+        TBL.rows[row * 4 + 3].cells[col * 5 + 3].style.height = "20mm";
+      } else if (row_length === 3) {
+        TBL.rows[row * 4 + 3].cells[col * 5 + 3].style.height = "38mm";
+      }
 
-      const a = left_array[col * 5 + row];
-      const b = right_array[col * 5 + row];
+      const a = left_array[row * 3 + col];
+      const b = right_array[row * 3 + col];
+
       if (Math.floor(a / 100) != 0) TBL.rows[row * 4 + 1].cells[col * 5 + 1].innerHTML = Math.floor(a / 100);
       else TBL.rows[row * 4 + 1].cells[col * 5 + 1].innerHTML = "";
       TBL.rows[row * 4 + 1].cells[col * 5 + 2].innerHTML = Math.floor((a % 100) / 10);
